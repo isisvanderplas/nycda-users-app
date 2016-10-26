ed4eaeab36caf7f514ea06bd7ce047f1991f7517
Isis van der Plas <isisvanderplas@hotmail.com>
2016-10-26 15:58:55 +0200

    files added

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+      pug = require('pug');
+      morgan = require('morgan');
+      fs = require('fs');
+
+var app = express();
+  userStore = JSON.parse(fs.readFileSync('users.json'));
+
+app.use(morgan('dev'));
+
+app.set('view engine', 'pug');
+
+app.get('/', (request, response) => {
+  response.render('index', { users: userStore });
+});
+
+app.listen(3000, () => {
+  console.log('Web Server is running on port 3000');
+});
