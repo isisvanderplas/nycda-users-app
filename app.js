e7274990dd4d5728362a8225d0a7cae779258105
Isis van der Plas <isisvanderplas@hotmail.com>
2016-10-26 16:40:58 +0200

    added searchbar

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get('/', (request, response) => {
   response.render('index', { users: userStore });
 });
 
+app.get('/search', (request, response) => {
+  response.render('searchbar');
+});
+
 app.listen(3000, () => {
   console.log('Web Server is running on port 3000');
 });
